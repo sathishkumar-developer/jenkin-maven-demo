ffbfcb488751d7b7c042056207aaf914e969001b
sathish3387 <sathish.socgen@gmail.com>
2024-05-11 12:32:03 +0530

    my first commit

---
+++ src/main/java/com/jenkins/example/mavenjenkinsdemo/MavenJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.example.mavenjenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenJenkinsDemoApplication.class, args);
+	}
+
+}
